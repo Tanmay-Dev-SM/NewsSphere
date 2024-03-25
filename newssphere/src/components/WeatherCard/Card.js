a0bdf3055f3f48556b2c474fd7967334f1fa9f25
Vamsi Kolli <vamsikolli@Vamsis-Laptop.local>
2024-03-25 18:51:58 -0400

    weather api implementation

--- newssphere/src/components/WeatherCard/Card.js
+++ newssphere/src/components/WeatherCard/Card.js
@@ -26,24 +26,24 @@ const WeatherCard = ({ weatherData }) => {
       </div>
       <div className="flex">
         <p className="day">
-          {moment().format("dddd")}, <span>{moment().format("LL")}</span>
+          {moment()?.format("dddd")}, <span>{moment()?.format("LL")}</span>
         </p>
-        <p className="description">{location?.weather[0].main}</p>
+        <p className="description">{location?.weather?.[0].main}</p>
       </div>
 
       <div className="flex">
-        <p className="temp">Temp. {location?.main.temp} &deg;F</p>
-        <p className="temp">Humidity {location?.main.humidity} %</p>
+        <p className="temp">Temp. {location?.main?.temp} &deg;F</p>
+        <p className="temp">Humidity {location?.main?.humidity} %</p>
       </div>
 
       <div className="flex">
         <p className="sunrise-sunset">
           Sunrise:{" "}
-          {new Date(location?.sys.sunrise * 1000).toLocaleTimeString("en-US")}
+          {new Date(location?.sys?.sunrise * 1000)?.toLocaleTimeString("en-US")}
         </p>
         <p className="sunrise-sunset">
           Sunset:{" "}
-          {new Date(location?.sys.sunset * 1000).toLocaleTimeString("en-US")}
+          {new Date(location?.sys?.sunset * 1000)?.toLocaleTimeString("en-US")}
         </p>
       </div>
     </div>
